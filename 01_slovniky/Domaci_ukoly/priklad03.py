323c3bc084703be3fd1b09dae7964747949c8acd
blannik <bl.novakova@gmail.com>
2021-02-19 10:35:16 +0100

    Domácí úkol 3: Zasedačky

---
+++ 01_slovniky/Domaci_ukoly/priklad03.py
@@ -0,0 +1,9 @@
+volnePokoje = {
+  9: ["Amadeus", "Goya", "Vlasy"],
+  10: ["Forman", "Goya"],
+  11: [],
+  12: ["Amadeus", "Vlasy"]
+}
+
+hodina = int(input("Zadej hodinu: "))
+print(volnePokoje[hodina])
